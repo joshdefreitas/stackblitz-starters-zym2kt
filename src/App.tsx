1737949626892dffa9365c77426d56d70fd1ee5e
joshdefreitas <joshdefreitas@gmail.com>
2023-10-22 18:59:27 -0400

    added Person

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
 
 export const App: FC<{ name: string }> = ({ name }) => {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
   return (
-    <Greet name='Josh' messageCount={10} isLoggedIn = {true}/>
+    <div>
+      <Greet name='Josh' messageCount={10} isLoggedIn = {true}/>
+      <Person name={personName}/>
+    </div>
   );
 };
  
